3ebd692750ce0c2232205e2bd825cceaf633eb94
Litanontaru <litanontaru@yahoo.com>
2015-08-18 12:23:00 +0400

    fix spring

--- bigdata/spring/src/main/java/com/epam/spring/Config.java
+++ bigdata/spring/src/main/java/com/epam/spring/Config.java
@@ -1,7 +1,5 @@
 package com.epam.spring;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
-    private static Logger logger = LoggerFactory.getLogger(Config.class);
-
     @Bean
     public Girl kleo() {
         return new Girl();
@@ -22,13 +18,7 @@ public class Config {
     }
 
     public static void main(String[] args) {
-        logger.debug("Start");
-        ApplicationContext context = null;
-        try {
-            context = new AnnotationConfigApplicationContext(Config.class);
-            context.getBean(Girl.class).think();
-        } catch (Exception e) {
-            logger.error("", e);
-        }
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        context.getBean(Girl.class).think();
     }
 }
