c2a855937c5ea78b107ac235e24fcfc6c267193b
Litanontaru <litanontaru@yahoo.com>
2015-08-19 16:24:34 +0400

    test on mapper processor logic

---
+++ bigdata/mapreduce/src/test/java/com/epam/mapreduce/MapperProcessorUTest.java
@@ -0,0 +1,37 @@
+package com.epam.mapreduce;
+
+import com.epam.common.LengthPredicate;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
+
+@Test
+public class MapperProcessorUTest {
+    public static final String INPUT = "Towards the end of November, during a thaw, at nine o'clock one morning";
+    public static final String[] OUTPUT = new String[] {
+            "towards",
+            "november",
+            "during",
+            "thaw",
+            "nine",
+            "clock",
+            "morning"
+    };
+
+    public void test() {
+        MapperProcessor processor = new MapperProcessor(new LengthPredicate());
+
+        Iterator<String> process = processor.process(INPUT);
+        for (String s : OUTPUT) {
+            assertTrue(process.hasNext());
+            String next = process.next();
+            assertEquals(next, s);
+        }
+        assertFalse(process.hasNext());
+    }
+}
