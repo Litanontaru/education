aef59753139b37d7149918770f83b0776467b499
Litanontaru <litanontaru@yahoo.com>
2015-08-19 12:56:40 +0400

    code style

--- bigdata/mapreduce/src/run/run.sh
+++ bigdata/mapreduce/src/run/run.sh
@@ -10,20 +10,21 @@ unzip $1-dep.zip
 #form libs
 libjars=
 first=1
+
 for f in lib/*.jar; 
 do
-  if [ $first -eq 1 ]; then
-    first=0;
-  else
-	libjars=$libjars','
-  fi
-  libjars=$libjars$f;
+    if [ $first -eq 1 ]; then
+        first=0;
+    else
+	    libjars=$libjars','
+    fi
+    libjars=$libjars$f;
 done
 
 #remove old output
 if hadoop fs -test -d $4; then
-  echo 'remove '$4;
-  hadoop fs -rm -r $4;
+    echo 'remove '$4;
+    hadoop fs -rm -r $4;
 fi
 
 #run hadoop
