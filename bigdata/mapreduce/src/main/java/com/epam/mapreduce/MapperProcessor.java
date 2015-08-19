975ae86675c0ff1a0f6bd528f086e06f4fde4a36
Litanontaru <litanontaru@yahoo.com>
2015-08-19 16:24:19 +0400

    mapper processor

---
+++ bigdata/mapreduce/src/main/java/com/epam/mapreduce/MapperProcessor.java
@@ -0,0 +1,66 @@
+package com.epam.mapreduce;
+
+import com.epam.common.LengthPredicate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MapperProcessor {
+    private final LengthPredicate predicate;
+
+    @Autowired
+    public MapperProcessor(LengthPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    public Iterator<String> process(String s) {
+        return new WordIterator(predicate, s);
+    }
+
+    private static class WordIterator implements Iterator<String> {
+        private static final Pattern PATTERN = Pattern.compile("[A-Za-z]([A-Za-z-]?[A-Za-z]+)*");
+
+        private final LengthPredicate predicate;
+
+        private final Matcher matcher;
+        private int s;
+        private boolean needToMove;
+
+        public WordIterator(LengthPredicate predicate, String string) {
+            this.predicate = predicate;
+            matcher = PATTERN.matcher(string);
+            s = 0;
+            needToMove = true;
+        }
+
+        private void move() {
+            if (needToMove) {
+                while (matcher.find(s)) {
+                    s = matcher.end();
+                    String group = matcher.group();
+                    if (predicate.test(group)) {
+                        needToMove = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public boolean hasNext() {
+            move();
+            return !needToMove;
+        }
+
+        public String next() {
+            move();
+            needToMove = true;
+            return matcher.group().toLowerCase();
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
