8d10b4e3e67f679eb237d2c54f00b29e73811cb6
litanontaru <litanontaru@yahoo.com>
2015-11-17 10:36:26 +0400

    fix  NPE

--- bigdata/hw6/src/main/java/com/epam/hive/UAParser.java
+++ bigdata/hw6/src/main/java/com/epam/hive/UAParser.java
@@ -18,14 +18,18 @@ import org.apache.hadoop.io.MapWritable;
                 "  > SELECT uamap(ua) FROM logdata a;\n"
 )
 public class UAParser extends UDF {
+    private static final MapWritable EMPTY = new MapWritable();
+    private static final Text UA_TYPE = new Text("uatype");
+    private static final Text UA_FAMILY = new Text("uafamily");
+    private static final Text OS_NAME = new Text("osname");
+    private static final Text DEVICE = new Text("device");
+
     private final UserAgentStringParser parser = UADetectorServiceFactory.getResourceModuleParser();
 
-    private final Text UA_TYPE = new Text("uatype");
-    private final Text UA_FAMILY = new Text("uafamily");
-    private final Text OS_NAME = new Text("osname");
-    private final Text DEVICE = new Text("device");
-
     public MapWritable evaluate(Text s) {
+        if (s == null) {
+            return EMPTY;
+        }
         MapWritable result = new MapWritable();
         ReadableUserAgent userAgent = parser.parse(s.toString());
         result.put(UA_TYPE, new Text(userAgent.getType().getName()));
@@ -33,9 +37,5 @@ public class UAParser extends UDF {
         result.put(OS_NAME, new Text(userAgent.getOperatingSystem().getName()));
         result.put(DEVICE, new Text(userAgent.getDeviceCategory().getCategory().getName()));
         return result;
-    }
-
-    public static void main(String[] args) {
-        new UAParser().evaluate(new Text(""));
     }
 }
