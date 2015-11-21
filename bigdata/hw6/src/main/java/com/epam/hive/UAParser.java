fc4325bdf2764872044f065ba5885acfe29b28fc
Andrei_Yakushin <Andrei_Yakushin@epam.com>
2015-11-21 20:55:17 +0400

    made hw6

--- bigdata/hw6/src/main/java/com/epam/hive/UAParser.java
+++ bigdata/hw6/src/main/java/com/epam/hive/UAParser.java
@@ -7,10 +7,6 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.MapWritable;
 
-/**
- * Created by litan_000 on 16.11.2015.
- */
-
 @Description(
         name = "uamap",
         value = "_FUNC_(str) - Parse a string to user agent data",
@@ -18,24 +14,17 @@ import org.apache.hadoop.io.MapWritable;
                 "  > SELECT uamap(ua) FROM logdata a;\n"
 )
 public class UAParser extends UDF {
-    private static final MapWritable EMPTY = new MapWritable();
-    private static final Text UA_TYPE = new Text("uatype");
-    private static final Text UA_FAMILY = new Text("uafamily");
-    private static final Text OS_NAME = new Text("osname");
-    private static final Text DEVICE = new Text("device");
+    private static final Text EMPTY = new Text("\t\t\t");
 
     private final UserAgentStringParser parser = UADetectorServiceFactory.getResourceModuleParser();
 
-    public MapWritable evaluate(Text s) {
+    public Text evaluate(Text s) {
         if (s == null) {
             return EMPTY;
         }
-        MapWritable result = new MapWritable();
-        ReadableUserAgent userAgent = parser.parse(s.toString());
-        result.put(UA_TYPE, new Text(userAgent.getType().getName()));
-        result.put(UA_FAMILY, new Text(userAgent.getFamily().getName()));
-        result.put(OS_NAME, new Text(userAgent.getOperatingSystem().getName()));
-        result.put(DEVICE, new Text(userAgent.getDeviceCategory().getCategory().getName()));
-        return result;
+        ReadableUserAgent ua = parser.parse(s.toString());
+        return new Text(
+                ua.getType().getName() + "\t" + ua.getFamily().getName() + "\t" + ua.getOperatingSystem().getName() + "\t" + ua.getDeviceCategory().getCategory().getName()
+        );
     }
 }
