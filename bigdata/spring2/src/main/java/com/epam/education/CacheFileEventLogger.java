4f923a90c82f966a207de7ef8b1bdeca01d19e12
Andrei_Yakushin <Andrei_Yakushin@epam.com>
2016-05-08 11:07:07 +0400

    added changes for part #1.6 and fixed bugs

--- bigdata/spring2/src/main/java/com/epam/education/CacheFileEventLogger.java
+++ bigdata/spring2/src/main/java/com/epam/education/CacheFileEventLogger.java
@@ -1,5 +1,6 @@
 package com.epam.education;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,7 +9,7 @@ import java.util.List;
  */
 public class CacheFileEventLogger extends FileEventLogger {
     private int cacheSize;
-    private List<Event> cache;
+    private List<Event> cache = new ArrayList<Event>();
 
     public CacheFileEventLogger(String filename) {
         super(filename);
